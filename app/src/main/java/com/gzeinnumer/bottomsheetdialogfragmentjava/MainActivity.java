2c096a9b4c82a243dadbe80032b64fd69f88dcbd
M. Fadli Zein <45892408+gzeinnumer@users.noreply.github.com>
2021-10-31 18:53:54 +0700

    Initial commit

---
+++ app/src/main/java/com/gzeinnumer/bottomsheetdialogfragmentjava/MainActivity.java
@@ -0,0 +1,27 @@
+package com.gzeinnumer.bottomsheetdialogfragmentjava;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button btn = findViewById(R.id.open_bottom_sheet);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                BottomSheetDialog bottomSheet = new BottomSheetDialog();
+                bottomSheet.show(getSupportFragmentManager(),
+                        "ModalBottomSheet");
+            }
+        });
+    }
+}
